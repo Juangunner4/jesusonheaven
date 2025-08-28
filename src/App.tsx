c5de01d9918f527bedfb69ec94d8a936e5569f55
Juan Vazquez <juanje1019@gmail.com>
2025-08-28 16:23:23 -0400

    Maybe this one works

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import './App.css';
 import Home from './pages/Home';
+import backgroundHeaven from './assets/backgroundheaven.svg';
 
 function App() {
+  React.useEffect(() => {
+    document.documentElement.style.setProperty('--background-heaven', `url(${backgroundHeaven})`);
+  }, []);
+
   return (
     <div className="App">
       <Home />
