4dcd9d058b064b60ae041827c88360c5037d4a1b
Juan Vazquez <juanje1019@gmail.com>
2025-08-28 16:30:51 -0400

    New png

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Home from './pages/Home';
-import backgroundHeaven from './assets/backgroundheaven.svg';
+import backgroundHeaven from './assets/backgroundheaven.png';
 
 function App() {
   React.useEffect(() => {
